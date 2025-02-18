53558238259cb67a473d40690d5ed9afcd9579fd
oliviawu <oliviawu0301@gmail.com>
2025-02-18 16:30:09 +0800

    創建版本11

---
+++ example-11/resources/views/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>hello</h3>
+    <p>
+        <a href="f1">f1</a>
+    </p>
+    <p>
+        <a href="f2">f2</a>
+    </p>
+    <p>
+        <a href="f3">f3</a>
+    </p>
+</body>
+
+</html>
--- example-11/routes/web.php
+++ example-11/routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('f1', function () {
+    return view('f1');
+});
+
+Route::get('f2', function () {
+    return view('f2');
+});
+
+Route::get('f3', function () {
+    return view('f3');
+});
+
+
+Route::get('hello', function () {
+    return view('hello');
 });
